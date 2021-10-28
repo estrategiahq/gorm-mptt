69336f5af71fe3b68fe0c354acf8629b0798e1b8
golgher <danielgolgher@gmail.com>
2021-10-28 12:20:05 -0300

    teste

--- func.go
+++ func.go
@@ -1,20 +1,8 @@
 package gorm_mptt
 
-import (
-	"fmt"
-	"reflect"
-)
-
-func (db *Tree) GetNode(o interface{}) interface{} {
-	rv := reflect.ValueOf(o)
-	rv_id := rv.FieldByName("ID").String()
-
-	var id string
-	id = fmt.Sprintf("%s", rv_id)
-	fmt.Println(id)
+func (db *Tree) GetNode(o interface{}, id string) interface{} {
 	db.Statement.First(&o, map[string]interface{}{"id": id})
 	return o
-
 }
 
 func (db *Tree) getMax(o interface{}) (interface{}, error) {
