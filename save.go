b8a3071958fdc352b1031c0e87a0d4c53d9db658
golgher <danielgolgher@gmail.com>
2021-10-30 12:36:51 -0300

    teste

--- save.go
+++ save.go
@@ -1,7 +1,6 @@
 package gorm_mptt
 
 import (
-	"fmt"
 	"reflect"
 )
 
@@ -26,9 +25,9 @@ func (db *Tree) SaveNode(o interface{}) (interface{}, error) {
 		rv.FieldByName("Lft").SetInt(edge)
 		rv.FieldByName("Rght").SetInt(edge + 1)
 
-		cond := fmt.Sprintf(">= %d", edge)
+		// cond := fmt.Sprintf(">= %d", edge)
 
-		db.sync(o, 2, "+", cond)
+		// db.sync(o, 2, "+", cond)
 	}
 
 	err := db.Statement.Create(o).Error
